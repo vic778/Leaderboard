51f88268e5d11dcb579a12c0121c79c164fedcaf
vic778 <victoiremmanuelbarh@gmail.com>
2021-12-12 17:47:55 +0200

    Created data

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './style.css';
+import data from './data.js';
+
+const scoreTable = document.querySelector('#score-details');
+
+const renderBoard = async () => {
+  const scores = data;
+  scoreTable.innerHTML = '';
+  scores.forEach((item) => {
+    const newLi = `<p class="p-name score-elements">${item.user}: ${item.score}</p>`;
+    scoreTable.innerHTML += newLi;
+  });
+};
+
+renderBoard();
